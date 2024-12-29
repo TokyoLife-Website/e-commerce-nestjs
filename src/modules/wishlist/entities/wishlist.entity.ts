c98c6d6a87f165a950c69cd689916e7f8fd02fd1
Le Minh Tien <leminhtien4323@gmail.com>
2024-12-29 18:56:48 +0700

    feat: implement wishlist api

--- src/modules/wishlist/entities/wishlist.entity.ts
+++ src/modules/wishlist/entities/wishlist.entity.ts
@@ -1,19 +1,18 @@
 import { Product } from 'src/modules/products/entities/product.entity';
 import { User } from 'src/modules/users/entities/user.entity';
-import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class WishList {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @ManyToOne(() => User, (user) => user.wishlist, {
-    eager: true,
     onDelete: 'CASCADE',
   })
   @JoinColumn({ name: 'userId' })
   user: User;
 
-  @ManyToOne(() => Product, { eager: true, onDelete: 'CASCADE' })
+  @ManyToOne(() => Product, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'productId' })
   product: Product;
 }
--- src/modules/wishlist/wishlist.module.ts
+++ src/modules/wishlist/wishlist.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WishlistController } from './wishlist.controller';
 import { WishlistService } from './wishlist.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WishList } from './entities/wishlist.entity';
+import { ProductsModule } from '../products/products.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([WishList]), UsersModule, ProductsModule],
   providers: [WishlistService],
   controllers: [WishlistController],
 })
